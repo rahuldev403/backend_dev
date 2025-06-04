d11bc1db19f431adef11ef86589de25810e51d0f
Rahul Swain <level432520537352822@gmail.com>
2025-06-04 17:47:35 +0530

    connected database

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log("hellow world")
+import "dotenv/config";
+import connectDb from "./db/index.js";
+import { DB_NAME } from "./constants.js";
+
+console.log(process.env.MONGODB_URI, DB_NAME);
+connectDb();
